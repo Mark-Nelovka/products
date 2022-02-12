1da4d8ea6c5211eda78b3697227788914a524b35
Mark-Nelovka <87484591+Mark-Nelovka@users.noreply.github.com>
2022-02-12 17:32:30 +0200

    add top card

---
+++ src/ProductOne/ImageOne.js
@@ -0,0 +1,7 @@
+// import React from 'react';
+
+function ImageOne() {
+  return <img src={"../images/image1-min.png"} alt="Sdsvs" />;
+}
+
+export default ImageOne;
---
+++ src/ProductOne/ProductOne.js
@@ -0,0 +1,49 @@
+import { Component } from "react";
+// import ImageOne from './ImageOne'
+import images from "../images/image1-min.png";
+import vactorScale from "../images/scales-min.svg";
+import s from "./ProductOne.module.css";
+
+class ProductCardOne extends Component {
+  state = {};
+  render() {
+    return (
+      <>
+        <div className={s.card}>
+          <div className={s.ContainerNew}>
+            <p className={s.new}>NEW</p>
+          </div>
+          <div className={s.img}>
+            <img
+              src={images}
+              alt={"Мицелярная вода"}
+              width={"256px"}
+              height={"261px"}
+            />
+            {/* <ImageOne /> */}
+          </div>
+          <div className={s.containerScale}>
+            <img
+              src={vactorScale}
+              alt={"Scale"}
+              width={"16px"}
+              height={"16px"}
+            />
+          </div>
+          <div className={s.containerProductName}>
+            <p className={s.productName}>Шампунь</p>
+          </div>
+          <div className={s.containerDescription}>
+            <p className={s.descriptionProduct}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco.
+            </p>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default ProductCardOne;
